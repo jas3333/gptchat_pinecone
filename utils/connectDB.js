37fecb212fa91550bdd19df2bf4ed2c61c05030d
jas3333 <accounts1@protonmail.com>
2023-03-11 09:17:22 -0800

    removed /memories from db connection in connectDB.js Full path should be placed in the .env

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        const connect = await mongoose.connect(`${process.env.MONGODB}/memories`);
+        const connect = await mongoose.connect(`${process.env.MONGODB}`);
         console.log(`MongoDB connected ${connect.connection.host}`);
     } catch (error) {
         console.log(error);
