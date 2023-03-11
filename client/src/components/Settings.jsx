218865f3580ada4985654e9b723db31477f9f379
jas3333 <accounts1@protonmail.com>
2023-03-10 23:35:51 -0800

    Added tooltips to settings menu

--- client/src/components/Settings.jsx
+++ client/src/components/Settings.jsx
@@ -30,6 +30,7 @@ const Settings = memo(({ botSettings, settingsChange, reset }) => {
                         name='tokens'
                         value={botSettings.tokens}
                         onChange={settingsChange}
+                        title="Sets the token limit. While the max is around 4k tokens, it will reject the api call if it's set too high."
                     />
                     <label className='value-label' htmlFor='tokens'>
                         Temperature: {botSettings.temperature}
@@ -40,11 +41,12 @@ const Settings = memo(({ botSettings, settingsChange, reset }) => {
                         id='tokens'
                         type='range'
                         min='0'
-                        max='1'
+                        max='2'
                         step='.1'
                         name='temperature'
                         value={botSettings.temperature}
                         onChange={settingsChange}
+                        title='What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic. Best set to 0.'
                     />
                     <label className='value-label' htmlFor='tokens'>
                         Top_P: {botSettings.top_p}
@@ -59,6 +61,7 @@ const Settings = memo(({ botSettings, settingsChange, reset }) => {
                         name='top_p'
                         value={botSettings.top_p}
                         onChange={settingsChange}
+                        title='Adjust the top_p parameter to control the diversity of generated text. A higher value results in more diverse text, while a lower value results in more predictable text.'
                     />
                 </div>
                 <div className='settings'>
@@ -76,6 +79,7 @@ const Settings = memo(({ botSettings, settingsChange, reset }) => {
                         name='presencePenalty'
                         value={botSettings.presencePenalty}
                         onChange={settingsChange}
+                        title="The presence_penalty setting is used in natural language processing to adjust the importance of presence in the context of a conversation. A high presence penalty means that the model will be less likely to generate a response that repeats or directly echoes the user's input. On the other hand, a low presence penalty means that the model will be more likely to generate a response that closely matches the user's input."
                     />
                     <label className='value-label' htmlFor='tokens'>
                         Frequency Penalty: {botSettings.frequencyPenalty}
@@ -91,11 +95,16 @@ const Settings = memo(({ botSettings, settingsChange, reset }) => {
                         name='frequencyPenalty'
                         value={botSettings.frequencyPenalty}
                         onChange={settingsChange}
+                        title='This setting is also used in natural language processing and adjusts the importance of word frequency in the context of a conversation. A high frequency penalty means that the model will be less likely to generate a response that includes frequently used words. On the other hand, a low frequency penalty means that the model will be more likely to generate a response that includes frequently used words.'
                     />
                 </div>
             </form>
             <div className='settings'>
-                <button onClick={reset} className='btn'>
+                <button
+                    onClick={reset}
+                    className='btn'
+                    title='Removes conversation saved in local storage and resets the conversation. The model still has access to conversations stored in the database.'
+                >
                     Reset
                 </button>
             </div>
