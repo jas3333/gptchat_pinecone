91b4695d92f85aae84f8d8b7aaefa0e8486222fc
jas3333 <accounts1@protonmail.com>
2023-04-01 14:15:54 -0700

    Added more logging to pineController

--- controllers/pineController.js
+++ controllers/pineController.js
@@ -3,14 +3,18 @@ import { getEmbeddings } from '../utils/tools.js';
 import { getMessages } from './../utils/tools.js';
 
 const queryPinecone = async (req, res) => {
+    const processLog = [];
     const query = req.body.query;
+
+    processLog.push(`Query: ${query}\n`);
 
     const vectors = await getEmbeddings(query);
     const pineconeResults = await queryIndex(vectors, 50);
-    const messages = await getMessages(pineconeResults, 0.79);
+    const messages = await getMessages(pineconeResults, 0.7);
 
-    console.log(messages);
+    processLog.push(`Returned Messages: ${[messages]}`);
 
+    console.log(processLog.join('\n'));
     res.status(200).json({ message: messages });
 };
 
