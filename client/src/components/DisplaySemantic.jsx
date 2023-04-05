334477c479966ff4f64174610dc82fcec32a6b73
jas3333 <accounts1@protonmail.com>
2023-04-05 10:44:53 -0700

    Fixes for search results formatting

--- client/src/components/DisplaySemantic.jsx
+++ client/src/components/DisplaySemantic.jsx
@@ -3,15 +3,30 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { coldarkDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 const DisplaySemantic = ({ item }) => {
+    const formattedItem = item.replace(/\\n/g, '\n');
+
+    const renderers = {
+        text: (props) => {
+            const { children, node } = props;
+            const { position } = node;
+            const sourceText =
+                position && position.start && position.end
+                    ? formattedItem.slice(position.start.offset, position.end.offset)
+                    : children;
+            return <span>{sourceText}</span>;
+        },
+    };
     return (
         <div>
             <div className='flex-box'>
                 <div>
                     <ReactMarkdown
-                        children={item}
+                        children={formattedItem}
+                        renderers={renderers}
                         components={{
                             code({ node, inline, className, children, ...props }) {
                                 const match = /language-(\w+)/.exec(className || '');
+
                                 return !inline && match ? (
                                     <SyntaxHighlighter
                                         children={String(children).replace(/\n$/, '')}
